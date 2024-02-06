832f9ad0e4206597e3467cd11ae2a803749f3193
james.sweeney <james.sweeney@liquidweb.com>
2024-02-06 13:04:23 -0600

    simplify hasError

--- src/lib/hasError.js
+++ src/lib/hasError.js
@@ -15,22 +15,10 @@ export default function hasError(field) {
     return '';
 
   // Get validity
-  const { validity } = field;
+  const { validity, validationMessage } = field;
 
   // If valid, return null
   if (validity.valid) return '';
 
-  // If field is required and empty
-  if (validity.valueMissing) return 'This field is required';
-
-  // If not the right type
-  if (validity.typeMismatch) {
-    // Email
-    if (field.type === 'email') return 'Please enter an email address';
-
-    // URL
-    if (field.type === 'url') return 'Please enter a URL';
-  }
-
-  return 'Something else';
+  return validationMessage;
 }
